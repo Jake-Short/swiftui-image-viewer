7e46687259fbf51a6ed8305a4d896445f7e21151
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2020-06-15 11:09:02 -0700

    Updated package naming

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftui-image-viewer",
+    name: "SwiftUIImageViewer",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "swiftui-image-viewer",
-            targets: ["swiftui-image-viewer"]),
+            name: "SwiftUIImageViewer",
+            targets: ["SwiftUIImageViewer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "swiftui-image-viewer",
+            name: "SwiftUIImageViewer",
             dependencies: [])
     ]
 )
