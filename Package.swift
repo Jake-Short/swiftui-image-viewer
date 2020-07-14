db84c3fa69475e10b5f1b923f35c96056c2fa673
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2020-07-14 11:02:22 -0700

    Attempt to fix package.swift

--- Package.swift
+++ Package.swift
@@ -20,14 +20,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "url-image", url: "https://github.com/dmytro-anokhin/url-image.git", .exact("0.9.15"))
+        .package(name: "URLImage", url: "https://github.com/dmytro-anokhin/url-image.git", .exact("0.9.15"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ImageViewer",
-            dependencies: ["url-image"]),
+            dependencies: ["URLImage"]),
         .target(
             name: "ImageViewerRemote",
             dependencies: [])
