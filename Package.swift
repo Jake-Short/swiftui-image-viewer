ae922b1a8fc1f657b1f7d3dc481585d8b5dbb638
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2020-07-14 13:17:39 -0700

    Reduced Swift tools to 5.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,16 +20,20 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "URLImage", url: "https://github.com/dmytro-anokhin/url-image.git", .exact("0.9.15"))
+        .package(url: "https://github.com/dmytro-anokhin/url-image.git", .exact("0.9.15"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ImageViewer",
-            dependencies: ["URLImage"]),
+            dependencies: [
+                .product(name: "URLImage", package: "url-image")
+            ]),
         .target(
             name: "ImageViewerRemote",
-            dependencies: [])
+            dependencies: [
+                .product(name: "URLImage", package: "url-image")
+            ])
     ]
 )
