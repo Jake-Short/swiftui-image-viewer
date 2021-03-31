de46d1ac2eb46925be74bcfc5fdfc29217c48bd4
Isvvc <lyons@tuta.io>
2021-03-31 12:28:53 -0600

    Move pinch to zoom to new file

--- Package.swift
+++ Package.swift
@@ -27,9 +27,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ImageViewer",
-            dependencies: ["URLImage"]),
+            dependencies: []),
         .target(
             name: "ImageViewerRemote",
-            dependencies: ["URLImage"])
+            dependencies: ["ImageViewer", "URLImage"])
     ]
 )
